2925ea85d0474630df67236ff3e11fccf72487e4
Kuldeepkant26 <picasuplayers@gmail.com>
2024-09-21 06:59:59 +0530

    Fixing css case issue

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../Css/nav.css';
+import '../css/nav.css';
 
 function Nav() {
     const navigate = useNavigate();
